020ee7aaeea1fed98584ee27f2b0df9d9e69c42e
ESp32 <lelebrr@gmail.com>
2025-11-29 17:38:34 -0300

    up
    up

--- lib/arduinojson/ArduinoJson.h
+++ lib/arduinojson/ArduinoJson.h
@@ -2,4 +2,16 @@
 // Copyright © 2014-2025, Benoit BLANCHON
 // MIT License
 
-#include "src/ArduinoJson.h"
+#pragma once
+
+#ifdef __cplusplus
+
+#  include "ArduinoJson.hpp"
+
+using namespace ArduinoJson;
+
+#else
+
+#error ArduinoJson requires a C++ compiler, please change file extension to .cc or .cpp
+
+#endif
